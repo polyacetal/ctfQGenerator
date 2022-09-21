69b4a29a99c0b1138209f0eed09a488163f510ff
polyacetal <yotyan988@gmail.com>
2022-09-22 01:41:08 +0900

    finish RandStr

--- RandStr/RandStr.py
+++ RandStr/RandStr.py
@@ -1,12 +1,24 @@
 import random
 import string
+import sys
 
 def Main():
-    filename = "question.txt"
-    key = "CTFKIT{well done}"
-    xMax = 1000
-    yMax = 1000
-    Process(filename, xMax, yMax, key)
+    args = sys.argv
+    del args[0]
+    ops = ["-f", "-k", "-x", "-y", "-h"]
+
+    if "-h" in args:
+        HelpPrint()
+    else:
+        arg = {}
+        for op in ops:
+            if op in args:
+                idx = args.index(op)
+                data = args[idx + 1]
+                arg[op] = data
+
+        filename, key, xMax, yMax = DecisionVar(arg)
+        Process(filename, xMax, yMax, key)
 
 def Process(filename, xMax, yMax, key):
     f = open(filename, 'w')
@@ -25,6 +37,43 @@ def Process(filename, xMax, yMax, key):
 def RandStr(n):
     randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
     return ''.join(randlst)
+
+def HelpPrint():
+    print("RandStr.pyの使い方")
+    print("-h: helpコマンド")
+    print("    これが含まれていると他に書かれていてもhelpを表示する\n")
+    print("-f: 出力するファイル名を指定する")
+    print("    デフォルトは\"question.txt\"です\n")
+    print("-k: 文字列に隠す文字列を指定する")
+    print("    デフォルトは\"CTFKIT{well done}\"です\n")
+    print("-x: 横幅の最大字数を指定する")
+    print("    デフォルトは1000文字です\n")
+    print("-y: 横幅の最大字数を指定する")
+    print("    少なくとも隠す文字の文字数以上を指定してください")
+    print("    デフォルトは1000文字です\n")
+
+def DecisionVar(arg):
+    if "-f" in arg.keys():
+        filename = arg["-f"]
+    else:
+        filename = "question.txt"
+
+    if "-k" in arg.keys():
+        key = arg["-k"]
+    else:
+        key = "CTFKIT{well done}"
+
+    if "-x" in arg.keys():
+        xMax = arg["-x"]
+    else:
+        xMax = 1000
+
+    if "-y" in arg.keys():
+        yMax = arg["-y"]
+    else:
+        yMax = 1000
+
+    return filename, key, xMax, yMax
 
 if __name__ == "__main__":
     Main()
