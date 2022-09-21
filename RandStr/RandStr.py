b472a7240d0e2c50f9e0c7bc3b2648927beb4d3d
polyacetal <yotyan988@gmail.com>
2022-09-21 12:03:44 +0900

    random string

--- RandStr/RandStr.py
+++ RandStr/RandStr.py
@@ -1,5 +1,24 @@
+import random
+import string
+
 def Main():
+    f = open('question.txt', 'w')
+    datalst = []
+    key = "CTFKIT{well done}"
+    x = random.randint(1,1000)
+    y = random.randint(1,1000 - len(key))
 
+    for i in range(1000):
+        if i == x:
+            datalst.append(RandStr(y) + key + RandStr(1000 - y - len(key)) + '\n')
+        datalst.append(RandStr(1000) + '\n')
 
-if __name__ = "__main__":
+    f.writelines(datalst)
+    f.close()
+
+def RandStr(n):
+    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
+    return ''.join(randlst)
+
+if __name__ == "__main__":
     Main()
